24f9fdb0a308a8e8f3ba1db05ca513d7ea817c85
Feride_queen <faridka0@gmail.com>
2022-12-18 21:04:40 +0300

    Это что-то

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿
+﻿// 29
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1, 100); 
 
-
-
-
+Console.WriteLine($"[{string.Join(", ", array)}]");
 
 
 
